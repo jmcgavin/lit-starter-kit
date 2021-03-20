2e34158c7d2de6755092284e65d05e44988d31c5
McJordan <39205937+jmcgavin@users.noreply.github.com>
2021-03-20 12:41:12 -0400

    chore: change default branch (#9)

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -2,19 +2,24 @@ import { css } from 'lit-element'
 
 export const GlobalStyles = css`
   :host {
-    /* General */
-    --app-white: #FFFFFA;
-    --app-grey: #757575;
-    --app-black: #131515;
+    /* Material Theme */
+    --mdc-typography-font-family: 'Roboto',Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+    --mdc-theme-primary: #3F51B5;
+    --mdc-theme-secondary: ##FF4081;
+    --mdc-theme-surface: #ffffff;
+    --mdc-theme-background: #253237;
+    --mdc-theme-on-primary: #ffffff;
+    --mdc-theme-on-secondary: #ffffff;
+    --mdc-theme-on-surface: #000000;
 
     /* Status */
+    --app-error: #F2545B;
+    --app-info: #00A1E4;
     --app-success: #1DB954;
     --app-warning: #F9CB40;
-    --app-error: #F2545B;
-
-    /* Text */
-    --app-light-text: var(--app-white);
-    --app-medium-text: var(--app-grey);
-    --app-dark-text: var(--app-black);
   }
+
+
+
+
 `
